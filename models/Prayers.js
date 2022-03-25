3de61529fb3b2efa0783832a6b5f2b41b36754a8
Amber Chunn <a.77@me.com>
2022-03-24 20:28:25 -0400

    Fixed MySQL Prayer Field Data Type

--- models/Prayers.js
+++ models/Prayers.js
@@ -1,32 +1,32 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Prayers extends Model {}
 
 // create data types for Prayers model
 Prayers.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      prayer: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'prayers'
-    }
-  );
+    prayer: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "prayers",
+  }
+);
 
-  module.exports = Prayers;
+module.exports = Prayers;
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
-const express = require('express');
-const sequelize = require('./config/connection');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const express = require("express");
+const sequelize = require("./config/connection");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-
-require('dotenv').config();
-
-const path = require('path');
+const path = require("path");
 const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-
 let cookieVar;
 // set up for running on heroku
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   cookieVar = process.env.JAWSDB_COOKIE;
 } else {
   cookieVar = process.env.DB_COOKIE;
@@ -27,23 +23,22 @@ if(process.env.JAWSDB_URL) {
 const sess = {
   secret: cookieVar,
   cookie: {
-    maxAge: (1000 * 60 * 60)
+    maxAge: 1000 * 60 * 60,
   },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sess));
 
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(routes);
 
